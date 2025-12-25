341328ef2f5ce12993ed7848691cb563014841fd
spidaDillon <dillontodd.dev@gmail.com>
2025-12-24 21:34:35 -0600

    remove comp type combo box

--- components/evaluation/comparables.tsx
+++ components/evaluation/comparables.tsx
@@ -231,19 +231,6 @@ export default function Comparables({
                   placeholder='365'
                 />
               </div>
-
-              <div className='space-y-2'>
-                <Label htmlFor='type'>Type</Label>
-                <select
-                  id='type'
-                  name='type'
-                  className='flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50'
-                  defaultValue='SALE'
-                >
-                  <option value='SALE'>Sales</option>
-                  <option value='RENT'>Rentals</option>
-                </select>
-              </div>
             </div>
 
             <Button type='submit' disabled={isPending}>
